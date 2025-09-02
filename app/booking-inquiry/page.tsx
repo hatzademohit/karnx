a8046c3a258d8777ae913dd423b1b7d49ea3d862
AzureAD\MukeshKshirsagar <mkshirsagar@valethi.com>
2025-09-02 09:57:49 +0530

    last changes update

--- app/booking-inquiry/page.tsx
+++ app/booking-inquiry/page.tsx
@@ -1,10 +1,11 @@
 import NewInquiry from "@/karnx/pages/BookingInquiry/BookingInquiry";
+import { StepProvider } from "../context/StepProvider";
 
 const page = () => {
     return(
-        <>
+        <StepProvider>
             <NewInquiry />
-        </>
+        </StepProvider>
     )
 }
 
