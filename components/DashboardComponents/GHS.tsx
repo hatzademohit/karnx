cf7d2472deff438f06b0800f1ffb752372d8d1e9
mhatzade@valethi.in <mhatzade@valethi.in>
2025-11-03 10:56:40 +0530

    GHS tab changes

--- components/DashboardComponents/GHS.tsx
+++ components/DashboardComponents/GHS.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 const GHS = () => {
     return(
         <>
-            <Box sx={{ p: 4, border: '1px solid #cccccc', backgroundColor: '#eeeeee', borderRadius: 2, alignContent: 'center', textAlign: "center" }}>
+            <Box sx={{ p: 4, border: '1px solid #cccccc', backgroundColor: '#f6f7ff', borderRadius: 2, alignContent: 'center', textAlign: "center" }}>
                 <Typography variant="h4">Comming soon</Typography>
             </Box>
         </>
