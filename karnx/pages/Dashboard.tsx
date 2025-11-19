0ec7cc2b9594e33317b9cbac2ba06a1561364475
mhatzade@valethi.in <mhatzade@valethi.in>
2025-11-19 12:34:13 +0530

    add InquiryDetailsContext

--- karnx/pages/Dashboard.tsx
+++ karnx/pages/Dashboard.tsx
@@ -3,38 +3,41 @@ import { CustomTabs } from "@/components";
 import { Box, Typography } from "@mui/material"
 import { KXManager, AircraftOperator, TravelAgent, GHS } from '@/components/DashboardComponents'
 import { useAuth } from "@/app/context/AuthContext";
+import { InquiryDetailsProvider } from "@/app/context/InquiryDetailsContext";
 
 const Dashboard = () => {
     const { user } = useAuth();
     return (
-        <Box>
-            <Typography component='h1' variant="h1" sx={{ mb: '10px' }}>Dashboard</Typography>
-            <CustomTabs defaultValue={0}>
-                {user.access_type === 'Portal Admin' &&
-                    <CustomTabs.Tab label="KX Manager">
-                        <KXManager />
-                    </CustomTabs.Tab>
-                }
+        <InquiryDetailsProvider>
+            <Box>
+                <Typography component='h1' variant="h1" sx={{ mb: '10px' }}>Dashboard</Typography>
+                <CustomTabs defaultValue={0}>
+                    {user.access_type === 'Portal Admin' &&
+                        <CustomTabs.Tab label="KX Manager">
+                            <KXManager />
+                        </CustomTabs.Tab>
+                    }
 
-                {user.access_type === 'Aircraft Operator' &&
-                    <CustomTabs.Tab label="Aircraft Operator">
-                        <AircraftOperator />
-                    </CustomTabs.Tab>
-                }
+                    {user.access_type === 'Aircraft Operator' &&
+                        <CustomTabs.Tab label="Aircraft Operator">
+                            <AircraftOperator />
+                        </CustomTabs.Tab>
+                    }
 
-                {user.access_type === 'Aircraft Travel Agent' &&
-                    <CustomTabs.Tab label="Travel Agent">
-                        <TravelAgent />
-                    </CustomTabs.Tab>
-                }
+                    {user.access_type === 'Aircraft Travel Agent' &&
+                        <CustomTabs.Tab label="Travel Agent">
+                            <TravelAgent />
+                        </CustomTabs.Tab>
+                    }
 
-                {user.access_type === 'Portal GHS' &&
-                    <CustomTabs.Tab label="GHS">
-                        <GHS />
-                    </CustomTabs.Tab>
-                }
-            </CustomTabs>
-        </Box>
+                    {user.access_type === 'Portal GHS' &&
+                        <CustomTabs.Tab label="GHS">
+                            <GHS />
+                        </CustomTabs.Tab>
+                    }
+                </CustomTabs>
+            </Box>
+        </InquiryDetailsProvider>
     )
 }
 
