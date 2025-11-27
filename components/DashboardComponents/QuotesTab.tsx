654991f4e789c66f034cf768e4bcab52ce8f3ba1
AzureAD\MukeshKshirsagar <mkshirsagar@valethi.com>
2025-11-27 10:00:01 +0530

    test

--- components/DashboardComponents/QuotesTab.tsx
+++ components/DashboardComponents/QuotesTab.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { ViewQuotes, NoQuotes } from "@/components";
+import { ViewQuotes, NoQuotes, QuoteDetails } from "@/components";
 import { useAuth } from "@/app/context/AuthContext";
 import { useInquiryDetails } from "@/app/context/InquiryDetailsContext";
+import { Grid } from "@mui/material";
 
 export interface QuoteTabsProps {
 	travelTab?: boolean;
@@ -12,13 +13,21 @@ const QuoteTabs: React.FC<QuoteTabsProps> = ({ travelTab = false, inquiryId }) =
 	const { user } = useAuth()
 	const { inquiryRowData } = useInquiryDetails();
 	const inqStsId = inquiryRowData?.status_id;
-	const statusIdViewQuote = [5, 10, 11, 12, 13, 14, 15, 6, 7, 8, 9];
+	const statusIdViewQuote = [5, 11, 12, 13, 14, 15, 6, 7, 8, 9];
 	const statusIdNoQuote = [1, 2, 3, 4];
+	const statusIdTravelAgentQuote = [10, 11, 12, 13, 14, 15];
 	return (
 		<>
-			{statusIdViewQuote.includes(inqStsId) &&
+			{statusIdViewQuote.includes(inqStsId) && user.access_type !== 'Aircraft Travel Agent' &&
 				<ViewQuotes />
 			}
+
+			{/* {user.access_type === 'Aircraft Travel Agent' && statusIdTravelAgentQuote.includes(inqStsId) &&
+				<Grid container spacing={{ md: 2, xs: 1 }}>
+					<QuoteDetails viewedQuote={[]} />
+				</Grid>
+
+			} */}
 			{statusIdNoQuote.includes(inqStsId) &&
 				<NoQuotes />
 			}
